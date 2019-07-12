4bc846a8eff5538a1892a572828f350157d7fb0c
BetterHireMe <hey@betterhire.me>
2019-07-12 19:29:05 +0300

    Less words

--- src/main/java/me/betterhire/letters/Main2.java
+++ src/main/java/me/betterhire/letters/Main2.java
@@ -8,15 +8,15 @@ public class Main2 {
         WordsChooser wordsChooser = new WordsChooser();
         WordsWriter wordsWriter = new WordsWriter();
 
-        List<String> wordsB = wordsChooser.choose(100);
+        List<String> wordsB = wordsChooser.choose(100   );
         wordsWriter.write("b_words.txt", wordsB);
         lettersCreator.createLetters("b_letters.txt", wordsB, 0.8);
 
-        List<String> wordsC = wordsChooser.choose(1000);
+        List<String> wordsC = wordsChooser.choose(500);
         wordsWriter.write("c_words.txt", wordsC);
         lettersCreator.createLetters("c_letters.txt", wordsC, 0.7);
 
-        List<String> wordsD = wordsChooser.choose(3000);
+        List<String> wordsD = wordsChooser.choose(1000);
         wordsWriter.write("d_words.txt", wordsD);
         lettersCreator.createLetters("d_letters.txt", wordsD, 0.65);
 
